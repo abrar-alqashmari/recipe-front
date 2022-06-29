e73b95d72f8bf2660014a7381fedd21275a4f601
man2ro <manar.ye,tr@gmail.com>
2022-06-29 16:12:16 +0300

    Main added

---
+++ src/screens/home/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './main.css'
+function Main() {
+  return (
+    <section className="content" >
+        <div className="content-wrap pb-0 overflow: visible;">
+        <div className='col-md-8 offset-md-2 center'>
+        <h2 class="text-title-light text-dark mb-5 ls1 text-uppercase">Hello &amp; Welcome.</h2>
+        <p className="pargraph">
+        I'm John Doe, Authoritatively communicate world-class alignments before low-risk high-yield growth strategies. Synergistically envisioneer transparent niches after dynamic "outside the box" thinking. Uniquely partnerships before synergistic niche markets.
+        </p>
+        </div>
+   </div>
+    </section>
+  
+      
+  )
+}
+
+export default Main

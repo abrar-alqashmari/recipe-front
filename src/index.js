afcff778cbb4f0a441b07da4992166a5b80381b6
Feras Jobeir <fjobeir@gmail.com>
2022-06-29 12:08:31 +0300

    setup (1)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/style.css'
+import './assets/css/dark.css'
+import './assets/css/font-icons.css'
+import './assets/css/colors.css'
+import './assets/css/fonts.css'
+import './assets/css/recipes.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)

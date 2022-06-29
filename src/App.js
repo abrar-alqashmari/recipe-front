9790519b78239e89995ef8ee0778b61415f272ec
Feras Jobeir <fjobeir@gmail.com>
2022-06-29 13:21:33 +0300

    Home Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./screens/home/Home";
 
 function App() {
 	return (
-		<div className="App">
-			
-		</div>
+		<>
+			<Header />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/category/:id' element={<Home />} />
+				<Route path='/recipe/:id' element={<Home />} />
+				<Route path="*" element={<>Not found</>} />
+			</Routes>
+			<Footer />
+		</>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/style.css'
@@ -13,6 +14,8 @@ import './assets/css/recipes.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )

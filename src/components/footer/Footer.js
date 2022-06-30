2cc322560a69f18a9177b2036ba73768edb56285
man2ro <manar.ye,tr@gmail.com>
2022-06-30 13:09:46 +0300

    Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,80 @@
+ import React from 'react';
+ import { link } from 'react-router-dom';
 const Footer = () => {
     return (
-        <></>
+        <>
+<div className="footer-widgets-wrap">
+<div className="row">
+<div className="col-lg-3 col-md-3 col-6 pe-4">
+ <div >
+<h4 className="ls0 nott">Download App</h4>
+{/* <link to="/"><img src="demos/articles/images/appstore.png" alt="App Link"/></link>
+<link to="/"><img src="demos/articles/images/googleplay.png" alt="App Link" className="mt-2"/></link>  */}
+</div> 
+</div>
+<div className="col-lg-3 col-md-3 col-6">
+ <div >
+ <h4 className="ls0 nott">Support</h4>
+ {/* <ul className="list-unstyled ms-0">
+<li className="text-black-50">Home</li>
+<li className="mb-2"><link to="/Home" className="text-black-50">About</link></li>
+ <li className="mb-2"><link to="/Home" className="text-black-50">FAQs</link></li> 
+ <li className="mb-2"><link to="/Home" className="text-black-50">Support</link></li>  
+</ul> */}
+</div> 
+</div>
+<div className="col-lg-3 col-md-3 col-6 mt-4 mt-md-0"> <div >
+ <h4 className="ls0 nott">Trending</h4>
+ {/* <ul className="list-unstyled ms-0">
+<li className="mb-2"><link to="/" className="text-black-50">Shop</link></li>
+<li className="mb-2"><link to="/" className="text-black-50">Portfolio</link></li>
+<li className="mb-2"><link to="/" className="text-black-50">Blog</link></li> 
+ </ul> */}
+</div>
+</div>
+<div className="col-lg-2 col-md-2 col-6 mt-4 mt-md-0">
+<div >
+ <h4 className="ls0 nott">Features</h4>
+  {/* <ul className="list-unstyled ms-0">
+  <li className="mb-2"><link to="/" className="text-black-50">Help Center</link></li>
+ <li className="mb-2"><link to="/" className="text-black-50">Paid with Moblie</link></li>
+ <li className="mb-2"><link to="/" className="text-black-50">Status</link></li>
+ <li className="mb-2"><link to="/" className="text-black-50">Contact Support</link></li> 
+ </ul> */}
+</div>
+</div>
+</div>
+</div>
+<div className="bg-light">
+ <div className="container">
+ <div className="row justify-content-center align-items-center">
+ <div className="col-sm-4 d-flex justify-content-sm-start mb-5 mb-sm-0 text-black-50">
+ <h5>Copyrights &copy; 2022 All Rights Reserved by MEM.</h5>
+ </div>
+</div>
+<div className="col-sm-4 d-flex justify-content-sm-center mb-3 mb-sm-0">
+ {/* <link to="https://facebook.com/semicolonweb" target="_blank" className="social-icon si-small si-colored si-facebook" title="Facebook">//<i className="icon-facebook"></i>
+<i className="icon-facebook"></i>
+</link>  */}
+ {/* <link to="https://twitter.com/__semicolon" target="_blank" className="social-icon si-small si-colored si-twitter" title="Twitter">
+<i className="icon-twitter"></i>
+<i className="icon-twitter"></i>
+</link>  */}
+{/* <link to="https://www.youtube.com/semicolonweb" target="_blank" className="social-icon si-small si-colored si-youtube" title="youtube">
+<i className="icon-youtube"></i>
+<i className="icon-youtube"></i>
+</link>  */}
+ {/* <link to="https://www.instagram.com/semicolonweb" target="_blank" className="social-icon si-small si-colored si-instagram" title="instagram">
+<i className="icon-instagram"></i>
+<i className="icon-instagram"></i>
+</link>  */}
+</div>
+<div className="col-sm-4 d-flex justify-content-sm-end">
+{/* <div className="copyright-links"><link to="/">Terms of Use</link> / <link to="/">Privacy Policy</link></div> */}
+</div>
+</div>
+</div>
+        </>
     )
 }
 

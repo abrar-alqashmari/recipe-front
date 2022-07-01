4ef51327e3fe16b66dad64f174bfce9b4fdf1b00
Mohammed Alhelali <alhelalimhmd750@gmail.com>
2022-07-01 14:30:35 +0300

    adding the signUser

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -2,45 +2,49 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Categories.css";
 
-export default function Categories() {
-  const [categories,setCategories]=useState([])
+const Categories = () => {
+  const [allCategories, setAllCategories] = useState([]);
+
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}categories`)
-            .then(response => {
-                response.json().then(categories => {
-                    if (categories?.success) {
-                        setCategories(categories.data)
-                    }
-                })
-            })
-            .catch(e => console.log(e))
-    }, [])
-  
+    const fetchData = async (err) => {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}categories`)
+      const resJson = await response.json()
+      setAllCategories(resJson.data)
+      if (err) {
+        console.error(err)
+      }
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <>
-    {categories && categories.map((category, i) => {
-      return (
-        <>
-        <div className="col-12 mt-5 mb-3">
-      <h3 className="center">Browse by Category</h3>
-      <div className="recipe-categories justify-content-center">
-      
-        <Link
-          to="demos/recipes/recipes.html"
-          data-animate="fadeInUp"
-          data-delay="100"
-          className="recipe-category"
-        >
-          <div className="recipe-category-inner">
-            <div className="recipe-category-icon">
-              <img src="" alt="Breakfast" />
-            </div>
-            <div className="recipe-category-info">Breakfast</div>
-          </div>
-        </Link>
-      </div>
-    </div>
-        </>
+      { allCategories && allCategories.map((data, i) => {
+       return (
+        
+          <div className="col-12 mt-5 mb-3">
+            <h3 className="center">Browse by Category</h3>
+            <div className="recipe-categories justify-content-center">
 
-    </>
-  );
+              <Link
+                to="demos/recipes/recipes.html"
+                data-animate="fadeInUp"
+                data-delay="100"
+                className="recipe-category"
+              >
+                <div className="recipe-category-inner">
+                  <div className="recipe-category-icon">
+                    <img src={data.icon} alt="Breakfast" />
+                  </div>
+                  <div className="recipe-category-info">Breakfast</div>
+                </div>
+              </Link>
+            </div>
+          </div>
+       );
+       })};
+    </> 
+  )
+}
+ export default Categories;

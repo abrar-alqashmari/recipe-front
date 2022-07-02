0d01e77dfd8a2d3399b172322c33ffb4a77213b2
man2ro <manar.ye,tr@gmail.com>
2022-07-02 10:47:21 +0300

    Categories Css

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,53 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Categories.css";
-// const Categories = () => {
-//   const [allCategories, setAllCategories] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async (err) => {
-//       const response = await fetch(`${process.env.REACT_APP_API_URL}categories`)
-//       const resJson = await response.json()
-//       setAllCategories(resJson.data)
-//       if (err) {
-//         console.error(err)
-//       }
-//     }
-//     fetchData()
-//   }, [])
-
-
-//   return (
-//     <>
-//       { allCategories && allCategories.map((data, i) => {
-//        return (
-        
-//           <div className="col-12 mt-5 mb-3">
-//             <h3 className="center">Browse by Category</h3>
-//             <div className="recipe-categories justify-content-center">
-
-//               <Link
-//                 to="demos/recipes/recipes.html"
-//                 data-animate="fadeInUp"
-//                 data-delay="100"
-//                 className="recipe-category"
-//               >
-//                 <div className="recipe-category-inner">
-//                   <div className="recipe-category-icon">
-//                     <img src={data.icon} alt="Breakfast" />
-//                   </div>
-//                   <div className="recipe-category-info">Breakfast</div>
-//                 </div>
-//               </Link>
-//             </div>
-//           </div>
-//        );
-//        })};
-//     </> 
-//   )
-// }
-//  export default Categories;
-
 export default function Categories() {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
@@ -63,10 +16,11 @@ export default function Categories() {
   }, []);
   console.log("hello1");
   return (
-    <div className="col-12 mt-5 mb-3">
-      <h3 className="center">Browse by Category</h3>
-      
+  <div>
+     <div className="col-12 mt-5 mb-3">
+         <h3 className="center">Browse by Category</h3>
       <div className="recipe-categories justify-content-center">
+  
         {categories.map((category, i) => {
           return (
             <div className={`category`} key={i}>
@@ -74,18 +28,16 @@ export default function Categories() {
                 to={`/category/${category.id}`}
                 data-animate="fadeInUp"
                 data-delay={i * 1000}
-                className="recipe-category"
-              >
-              <div className="col-12 mt-5 mb-3"></div>
+                className="recipe-category">
                 <div className="recipe-category-inner">
-                <div className={`categoryName`}>
+                <div className={`recipe-category-info`}>
                 <h5>{category.name}</h5>
                 </div>
-                  <div className={`categoryIcon`}>
+                  <div className={`recipe-category-icon`}>
                     <img src={category?.icon} alt={category.name} />
                   </div>
                   <div className={`categoryPhoto`}>
-                    <img src={category?.photo} alt={category.name} />
+                    <img src={category?.photo}  alt={category.name} />
                   </div>
                 </div>
               </Link>
@@ -93,6 +45,7 @@ export default function Categories() {
           );
         })}
       </div>
+    </div>
   </div>
   )
-    }
+   }

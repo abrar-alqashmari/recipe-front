d9440207c4d22fa45543c55fd64dfb09e768c1ec
abrar <iabrarali94@gmail.com>
2022-07-14 13:46:52 +0300

    singleRecipe

--- src/components/Account/user/AddRecipe.js
+++ src/components/Account/user/AddRecipe.js
@@ -6,117 +6,113 @@ import { useContext } from "react"
 import { AuthContext } from "../../../contexts/AuthContext"
 import Ingradients from "./Ingradients"
 const AddRecipe = () => {
-	const navigate = useNavigate()
-	const descriptionRef = useRef()
-	const videoRef = useRef()
-	const photoRef = useRef()
-	const backgroundRef = useRef()
-	const nameRef = useRef()
-	const [categories, setCategories] = useState([])
-	const [selectedCategories, setSelectedCategories] = useState([])
-	const handleCategoryToggle = (e) => {
-		const categoriesClone = [...selectedCategories]
-		if (e.target.checked) {
-			categoriesClone.push(e.target.value)
-		} else {
-			categoriesClone.splice(categoriesClone.indexOf(e.target.value), 1)
-		}
-		setSelectedCategories(categoriesClone)
-	}
-
-	useEffect(() => {
-		sendRequest(`${process.env.REACT_APP_API_URL}categories`)
-			.then((response) => {
-				setCategories(response?.data)
-			})
-	}, [])
-
-	const sendRequest = useRequest()
-	const ctx = useContext(AuthContext)
-	const addrecipe = () => {
-		const formdata = new FormData();
-		formdata.append('name', nameRef.current.value)
-		formdata.append('youtube_video', videoRef.current.value)
-		formdata.append('description', descriptionRef.current.value)
-		for (var i = 0; i < selectedCategories.length; i++) {
-			formdata.append('categories[]', selectedCategories)
-		}
-		formdata.append('recipe_photo', photoRef.current.files[0])
-		formdata.append('background_photo', backgroundRef.current.files[0])
-
-		sendRequest(`${process.env.REACT_APP_API_URL}recipes/addrecipe`, {}, formdata, {
-			auth: true
-		}, 'POST').then((response) => {
-			console.log(response)
-		})
-	}
-	return (
-		<>
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col">
-						<h2>Add New Recipe</h2>
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Name</label>
-						<input type={'text'} ref={nameRef} className='form-control' />
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Details</label>
-						<textarea ref={descriptionRef} className='form-control' cols="7"></textarea>
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Video</label>
-						<input type={'url'} ref={videoRef} className='form-control' />
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Photo</label>
-						<input type={'file'} ref={photoRef} className='form-control' />
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Background</label>
-						<input type={'file'} ref={backgroundRef} className='form-control' cols="7" />
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Categoreis</label>
-						<div className="row">
-						{categories?.map((category, i) => {
-							return (
-								<div className="col-4" key={i}>
-									<input  onChange={handleCategoryToggle} type={'checkbox'} value={category?.id} id={`category-${category.id}`} />
-									&nbsp;<label htmlFor={`category-${category.id}`}>{category.name}</label>
-								</div>
-							)
-						})}
-						</div>
-					</div>
-				</div>
-				<div className="row mb-3">
-					<div className="col">
-						<label>Recipe Ingradients</label>
-						<Ingradients />
-					</div>
-				</div>
-				<div className="row mt-4">
-					<div className="col d-flex justify-content-center">
-					<button onClick={addrecipe} type="button" className="button button-large m-0 fw-bold button-circle button-light">Submit</button>	
-					</div>	
-				</div>
-			</div>
-		</>
-	)
+    const navigate = useNavigate()
+    const descriptionRef = useRef()
+    const videoRef = useRef()
+    const photoRef = useRef()
+    const backgroundRef = useRef()
+    const nameRef = useRef()
+    const [categories, setCategories] = useState([])
+    const [selectedCategories, setSelectedCategories] = useState([])
+    const handleCategoryToggle = (e) => {
+        const categoriesClone = [...selectedCategories]
+        if (e.target.checked) {
+            categoriesClone.push(e.target.value)
+        } else {
+            categoriesClone.splice(categoriesClone.indexOf(e.target.value), 1)
+        }
+        setSelectedCategories(categoriesClone)
+    }
+    useEffect(() => {
+        sendRequest(`${process.env.REACT_APP_API_URL}categories`)
+            .then((response) => {
+                setCategories(response?.data)
+            })
+    }, [])
+    const sendRequest = useRequest()
+    const ctx = useContext(AuthContext)
+    const addrecipe = () => {
+        const formdata = new FormData();
+        formdata.append('name', nameRef.current.value)
+        formdata.append('youtube_video', videoRef.current.value)
+        formdata.append('description', descriptionRef.current.value)
+        for (var i = 0; i < selectedCategories.length; i++) {
+            formdata.append('categories[]', selectedCategories)
+        }
+        formdata.append('recipe_photo', photoRef.current.files[0])
+        formdata.append('background_photo', backgroundRef.current.files[0])
+        sendRequest(`${process.env.REACT_APP_API_URL}recipes/addrecipe`, {}, formdata, {
+            auth: true
+        }, 'POST').then((response) => {
+            console.log(response)
+        })
+    }
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <h2>Add New Recipe</h2>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Name</label>
+                        <input type={'text'} ref={nameRef} className='form-control' />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Details</label>
+                        <textarea ref={descriptionRef} className='form-control' cols="7"></textarea>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Video</label>
+                        <input type={'url'} ref={videoRef} className='form-control' />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Photo</label>
+                        <input type={'file'} ref={photoRef} className='form-control' />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Background</label>
+                        <input type={'file'} ref={backgroundRef} className='form-control' cols="7" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Categoreis</label>
+                        <div className="row">
+                        {categories?.map((category, i) => {
+                            return (
+                                <div className="col-4" key={i}>
+                                    <input  onChange={handleCategoryToggle} type={'checkbox'} value={category?.id} id={`category-${category.id}`} />
+                                    &nbsp;<label htmlFor={`category-${category.id}`}>{category.name}</label>
+                                </div>
+                            )
+                        })}
+                        </div>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
+                        <label>Recipe Ingradients</label>
+                        <Ingradients />
+                    </div>
+                </div>
+                <div className="row mt-4">
+                    <div className="col d-flex justify-content-center">
+                    <button onClick={addrecipe} type="button" className="button button-large m-0 fw-bold button-circle button-light">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
-
-export default AddRecipe 
+export default AddRecipe
--- src/components/recipes/SingelRecipe.js
+++ src/components/recipes/SingelRecipe.js
@@ -1,7 +1,7 @@
-
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
 const SingleRecipe = () => {
   const [singleRecipe, setSingleRecipe] = useState({});
   //   const [AllIngredients, setAllIngredients] = useState();
@@ -21,11 +21,148 @@ const SingleRecipe = () => {
     };
     fetchData();
   }, [id]);
-  console.log(singleRecipe?.Ingredients,"nice")
+
   return (
     <>
-    
-</>
+      <section
+        id="slider"
+        className="slider-element dark parallax include-header"
+        style={{
+          background: `url(${singleRecipe.background_photo}) center center / cover`,
+          padding: "230px",
+        }}
+      >
+        <div className="container clearfix">
+          <div className="mx-auto center" style={{ maxWidth: "800px" }}>
+            <h3 className="nott fw-bold mb-5 display-4">
+              {singleRecipe?.name}
+            </h3>
+          </div>
+          <div className="mx-auto center" style={{ maxWidth: "900px" }}>
+            <div className="slider-features">
+              <ul className="list-unstyled row g-0 align-items-center overflow-hidden col-mb-50 mt-5">
+                <li className="col-6 col-lg text-center text-lg-start">
+                  <div className="grid-inner px-5">
+                    <img
+                      src="demos/recipes/images/icons/level.svg"
+                      alt="Level"
+                      width="30"
+                    />
+                    <h5 className="mb-0">Beginner Level</h5>
+                  </div>
+                </li>
+                <li className="col-6 col-lg text-center text-lg-start">
+                  <div className="grid-inner px-5">
+                    <img
+                      src="demos/recipes/images/icons/timer.svg"
+                      alt="Timer"
+                      width="30"
+                    />
+                    <h5 className="mb-0">40 Mins</h5>
+                  </div>
+                </li>
+                <li className="col-6 col-lg text-center text-lg-start">
+                  <div className="grid-inner px-5">
+                    <img
+                      src="demos/recipes/images/icons/ingredients.svg"
+                      alt="Ingredients"
+                      width="30"
+                    />
+                    <h5 className="mb-0">9 Ingredients</h5>
+                  </div>
+                </li>
+                <li className="col-6 col-lg text-center text-lg-start">
+                  <div className="grid-inner px-5">
+                    <img
+                      src="demos/recipes/images/icons/star.svg"
+                      alt="Reviews"
+                      width="30"
+                    />
+                    <h5 className="mb-0">223 Reviews</h5>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="content" className="bg-light">
+        <div className="content-wrap pt-0" style={{ overflow: "visible" }}>
+          <div className="container">
+            <div className="card border-0 shadow-sm" style={{ top: "-100px" }}>
+              <div className="card-body px-4">
+                <div className="row justify-content-between mt-5 mb-4 clearfix">
+                  <div className="col-lg-4">
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <h4 className="mb-0">Ingredients</h4>
+                    </div>
+                    <ul className="list-unstyled list-ingredients bg-light p-4">
+                      {singleRecipe?.Ingredients?.map((recipe, i) => {
+                        return (
+                          <div>
+                            <li key={i}>{recipe.name}</li>
+                            <ul>
+                              <li>{recipe.description}</li>
+                            </ul>
+                          </div>
+                        );
+                      })}
+                    </ul>
+                    <div className="line line-sm"></div>
+                    <h4>Share:</h4>
+                    <a
+                      href="http://www.facebook.com"
+                      className="social-icon si-small si-colored si-facebook"
+                      title="Facebook"
+                    >
+                      <i className="icon-facebook"></i>
+                      <i className="icon-facebook"></i>
+                    </a>
+                    <a
+                      href="http://wwww.Twitter.com"
+                      className="social-icon si-small si-colored si-twitter"
+                      title="Twitter"
+                    >
+                      <i className="icon-twitter"></i>
+                      <i className="icon-twitter"></i>
+                    </a>
+                  </div>
+                  <div className="col-lg-8 mt-5 mt-lg-0">
+                    <h4>Video</h4>
+                    <iframe
+                      width="100%"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${singleRecipe?.youtube_video}`}
+                      title="YouTube video"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
+                    <div
+                      className="rounded position-relative dark mb-5"
+                      style={{
+                        background:
+                          "url('demos/recipes/images/recipe-week.jpg') no-repeat center center / cover; min-height: 300px",
+                      }}
+                    >
+                      <a
+                        href="https://www.youtube.com/watch?v=P3Huse9K6Xs"
+                        className="play-video stretched-link"
+                      >
+                        <i className="icon-play"></i>
+                      </a>
+                    </div>
+                    <h4>{singleRecipe.description}</h4>
+                    <ol className="list-unstyled list-preparation"></ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 export default SingleRecipe;
